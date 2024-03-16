2069530bab90a812a0beec752949704db19eafa8
Anish Kumar <anish.pcr@gmail.com>
2024-03-16 23:45:58 +0530

    Done Initialization

--- app/src/main/java/com/example/vm/activity_sales_entry.java
+++ app/src/main/java/com/example/vm/activity_sales_entry.java
@@ -2,6 +2,9 @@ package com.example.vm;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class activity_sales_entry extends AppCompatActivity {
 
+    Spinner to,productName;
+    EditText quantity,rate,sgstRate,cgstRate;
+    TextView hsnCode,intiAmount,gstAmount,finalAmount;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_entry);
+
+        to = findViewById(R.id.cpy_name);
+        productName = findViewById(R.id.productName);
+
+        hsnCode = findViewById(R.id.hsn);
+        intiAmount =findViewById(R.id.initialAmount);
+        gstAmount= findViewById(R.id.gstAmount);
+        finalAmount = findViewById(R.id.finalAmount);
+
+        quantity = findViewById(R.id.quantity);
+        rate= findViewById(R.id.rate);
+        sgstRate = findViewById(R.id.sgst);
+        cgstRate=findViewById(R.id.cgst);
+
+
 
     }
 
